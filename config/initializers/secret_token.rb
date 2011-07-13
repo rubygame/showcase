65c374338cf0c0d8b22128626521d183ae94f6b3
Allan Davis <cajun.code@gmail.com>
2011-07-12 23:32:39 -0400

    initalizing project to create showcase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showcase::Application.config.secret_token = 'e857363bb068d3e4d03be1aaaae07bcbd8221406a9d6caa761f827736901a777e6322adccc9a0053b92da007f85050718d6c67c786ca465d5e8af9df7a62adb0'
